68f14ebff8a58a7e58c720617d053c36c3eb0576
benthebear93 <nswve@naver.com>
2021-08-25 05:41:01 +0900

    few update

---
+++ tx90_gym/env/robots/tx90.py
@@ -0,0 +1,10 @@
+import numpy as np
+from gym import spaces
+
+from panda_gym.envs.core import PyBulletRobot
+
+class Tx90(PyBulletRobot):
+
+	def __init__(self, sim, base_position = [0, 0, 0]):
+		se
+
