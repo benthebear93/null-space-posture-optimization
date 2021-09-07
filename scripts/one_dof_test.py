c01ecef24968f6190a1de3dbe3e0749e776988b5
benthebear93 <nswve@naver.com>
2021-09-07 10:29:19 +0900

    ball added and Rz, euler added

--- scripts/one_dof_test.py
+++ scripts/one_dof_test.py
@@ -11,6 +11,8 @@ def deg2rad(degree):
 class Tx90():
     def __init__(self):
         self.URDFPath = "C:/Users/UNIST/Desktop/git/null-space-posture-optimization/urdf/tx90.urdf"
+        self.BallPath = "C:/Users/UNIST/Desktop/git/null-space-posture-optimization/urdf/ball.urdf"
+        self.BallPath2 = "C:/Users/UNIST/Desktop/git/null-space-posture-optimization/urdf/ball2.urdf"
         print(self.URDFPath)
         self.numofjoint = 6
         #Desktop\git\null-space-posture-optimization\urdf
@@ -23,11 +25,22 @@ class Tx90():
 
         robotStartPos = [0,0,0.0]
         robotStartOrn = p.getQuaternionFromEuler([0,0,0])
-        self.robotID = p.loadURDF(self.URDFPath, robotStartPos, robotStartOrn, useFixedBase = True,flags=p.URDF_USE_INERTIA_FROM_FILE)
-        self.eefID = 7
+        self.robotID = p.loadURDF(self.URDFPath, robotStartPos, robotStartOrn, useFixedBase = True, flags=p.URDF_USE_INERTIA_FROM_FILE)
+        self.eefID = 8
         self.joints, self.controlJoints = utils_tx90.setup_tx90(p, self.robotID) #get joints and controllable joints
+        #print(self.joints)
+        # print(self.joints.id)
         self.joint_init = [deg2rad(0), deg2rad(0), deg2rad(0), deg2rad(0), deg2rad(0), deg2rad(0)]
+        numJoints = p.getNumJoints(self.robotID)
+        for _id in range(numJoints):
+            _name = p.getJointInfo(self.robotID, _id)
+            print(_name)
+        print("numJ : ", numJoints)
+        ballStartPos = [0,0,0.0]
+        balltStartOrn = p.getQuaternionFromEuler([0,0,0])
 
+        ballId = p.loadURDF(self.BallPath, ballStartPos, balltStartOrn, useFixedBase = True, flags=p.URDF_USE_INERTIA_FROM_FILE)
+        
     def fk(self, jointpos):
         for i,name in enumerate(self.controlJoints):
             p.resetJointState(self.robotID, self.joints[name].id, targetValue=jointpos[i],targetVelocity=0)
@@ -42,9 +55,15 @@ class Tx90():
         ee_ori = np.array(ee_state[1])
         return ee_pos, ee_ori
     
+    def get_ee2(self):
+        ee_state = p.getLinkState(self.robotID, self.eefID-2)
+        ee_pos = np.array(ee_state[0])
+        ee_ori = np.array(ee_state[1])
+        return ee_pos, ee_ori
+
     def home_pose(self):
         for i, name in enumerate(self.controlJoints):
-            p.resetJointState(self.robotID,self.joints[name].id,targetValue=self.joint_init[i],targetVelocity=0)
+            p.resetJointState(self.robotID, self.joints[name].id,targetValue=self.joint_init[i],targetVelocity=0)
         p.stepSimulation()
 
     def inverse_kinematics(self, position, orientation):
@@ -64,6 +83,18 @@ class Tx90():
 
     def getQuaternionFromEuler(self, euler_ang):
         return p.getQuaternionFromEuler(euler_ang)
+    
+    def ballupdate(self, pos, ori):
+        ballStartPos = pos
+        balltStartOrn = p.getQuaternionFromEuler([0,0,0])
+
+        ballId = p.loadURDF(self.BallPath, ballStartPos, balltStartOrn, useFixedBase = True, flags=p.URDF_USE_INERTIA_FROM_FILE)
+
+    def ballupdate2(self, pos, ori):
+        ballStartPos = pos
+        balltStartOrn = p.getQuaternionFromEuler([0,0,0])
+
+        ballId = p.loadURDF(self.BallPath2, ballStartPos, balltStartOrn, useFixedBase = True, flags=p.URDF_USE_INERTIA_FROM_FILE)
 
 def Rx(theta):
   return np.array([[ 1, 0           , 0           ],
@@ -87,20 +118,22 @@ if __name__=="__main__":
     tx90.loadURDF()
     a = 10
     i = 0
-    tx90.fk([0, m.pi/3, -m.pi/2, 0, 0, 0])
+    tx90.fk([0.1338, 0.7443, deg2rad(83.18), deg2rad(44.5), deg2rad(-28.1), deg2rad(-55.54)])
+    # tx90.fk([0.0, 0.0, deg2rad(90), 0, 0, 0])
+    
     pos, ori = tx90.get_ee()
+    pos2, ori2 = tx90.get_ee2()
     print("pos: ", pos, "ori: ", ori)
     print("euler :", tx90.getEulerFromQuaternion(ori))
-    R = Rz(deg2rad(177.302))*Ry(deg2rad(88.824))*Rx(deg2rad(177.272))
-    for i in range(0,30):
-        pos, ori = tx90.get_ee() # get current ori
-        euler_ang = tx90.getEulerFromQuaternion(ori) # convert to euler
-        # print( "euler before: ", euler_ang)
-        euler_ang[0] = euler_ang[0] - deg2rad(5)
-        # print( "euler after: ", euler_ang)
-        quat = tx90.getQuaternionFromEuler(euler_ang)
-        j_pos = tx90.inverse_kinematics(pos, quat)
-        # print( "jpos: ", j_pos)
-        # print( "pos: ", pos)
+    tx90.ballupdate(pos,ori)
+    tx90.ballupdate2(pos2,ori2)
+    # R = np.dot(deg2rad(177.302), np.dot(Ry(deg2rad(88.824)), Rx(deg2rad(177.272))))
+    for i in range(10):
+        euler = tx90.getEulerFromQuaternion(ori)
+        euler[2] = euler[2] - deg2rad(1)
+        ori = tx90.getQuaternionFromEuler(euler)
+        j_pos = tx90.inverse_kinematics(pos, ori)
         tx90.fk(j_pos)
-        time.sleep(0.1)
+        print("after pos: ", pos, "after ori: ", ori)
+        time.sleep(0.5)
+    time.sleep(11000)
