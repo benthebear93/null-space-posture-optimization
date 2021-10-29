Jacobian fixed

conven_control/scripts/gradient_save.py
@@ -12,7 +12,7 @@ Ktheta = np.diag(np.array([1.7, 5.9, 1.8, 0.29, 0.93 ,0.49])) # 3x3
 root = os.getcwd()
 
 def external_force(): 
-    F = np.array([[1.0], [1.0], [20.0], [0.0], [0.0], [0.0]])
+    F = np.array([[-6.0], [-6.0], [40.0], [0.0], [0.0], [0.0]])
     return F
 
 def FK(joint_params):
@@ -157,6 +157,6 @@ if __name__ == "__main__":
     H_func = sym.lambdify([variables], target_f, modules='numpy')
 
     dill.settings['recurse'] = True
-    dill.dump(J_func, open(root+'\J_func_simp', "wb"))
-    dill.dump(Jn_func, open(root+'\Jn_func_simp', "wb"))
-    dill.dump(H_func, open(root+'\H_func_simp', "wb"))
+    dill.dump(J_func, open(root+'/J_func_simp', "wb"))
+    dill.dump(Jn_func, open(root+'/Jn_func_simp', "wb"))
+    dill.dump(H_func, open(root+'/H_func_simp', "wb"))

conven_control/scripts/pseudo_inverse.py
@@ -2,24 +2,30 @@ import time
 import numpy as np
 import sympy as sym
 import pandas as pd
+
 sym.init_printing()
 # from math import atan2, sqrt
 import matplotlib.pyplot as plt
+
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
 from spatialmath import *
 import dill
 import os
+
 root = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+
 
 def posture_read():
     # load_wb = load_workbook("C:/Users/UNIST/Desktop/stiffness_estimation/test_z.xlsx", data_only=True)
-    df = pd.read_excel('../data/random_curve_pos.xlsx', header=None, names=None, index_col=None)
-    num_test = df.shape[0]
+    df = pd.read_excel(
+        "../data/random_curve_pos.xlsx", header=None, names=None, index_col=None
+    )
+    testCount = df.shape[0]
 
-    print("number of test: ",  (num_test-1)/2)
-    overall_posval =[]
+    print("number of test: ", (testCount - 1) / 2)
+    overall_posval = []
     pos_val = []
-    for i in range(0, num_test):
+    for i in range(0, testCount):
         for j in range(0, 6):
             a = df.iloc[i][j]
             pos_val.append(a)
@@ -27,20 +33,29 @@ def posture_read():
         pos_val = []
     return overall_posval
 
+
 def is_success(error):
     accuracy = 0.001
-    if abs(error[0]) < 0.0005 and abs(error[1]) < 0.0005 and abs(error[2]) < 0.0005 and abs(error[3]) < 0.0001 and abs(error[4]) < 0.0001 and abs(error[5]) < 0.0001: 
+    if (
+        abs(error[0]) < 0.0005
+        and abs(error[1]) < 0.0005
+        and abs(error[2]) < 0.0005
+        and abs(error[3]) < 0.0001
+        and abs(error[4]) < 0.0001
+        and abs(error[5]) < 0.0001
+    ):
         return True
+
 
 def Joint_limit_check(q):
     if q[0] > 3.14159:
         q[0] = -3.14159 + q[0]
-    elif q[0] <-3.14159:
+    elif q[0] < -3.14159:
         q[0] = -q[0]
 
-    if q[1]> 2.5744:
+    if q[1] > 2.5744:
         q[1] = -2.57445 + q[1]
-    elif q[1] <-2.2689:
+    elif q[1] < -2.2689:
         q[1] = -q[1]
 
     if q[2] > 2.5307:
@@ -50,12 +65,12 @@ def Joint_limit_check(q):
 
     if q[3] > 4.7124:
         q[3] = -4.7124 + q[3]
-    elif q[3] <-4.7124:
+    elif q[3] < -4.7124:
         q[3] = -q[3]
 
     if q[4] > 2.4435:
         q[4] = -2.4435 + q[4]
-    elif q[4] <-2.0071:
+    elif q[4] < -2.0071:
         q[4] = -q[4]
 
     if q[5] > 4.7124:
@@ -65,120 +80,121 @@ def Joint_limit_check(q):
 
     return q
 
-def FK(joint_params):
+
+def FK(q):
     """
     Joint variables consisting of 6 parameters
     """
-    joint_params = np.asarray(joint_params, dtype=float)
-    q1, q2, q3, q4, q5, q6, q7 = joint_params
-    dh_param1 = np.array([0, 0.05, -pi/2]) # d a alpah
+    q = np.asarray(q, dtype=float)
+    q1, q2, q3, q4, q5, q6, q7 = q
+    dh_param1 = np.array([0, 0.05, -pi / 2])  # d a alpah
     dh_param2 = np.array([0, 0.425, 0])
-    dh_param3 = np.array([0.0534, 0, pi/2])
-    dh_param4 = np.array([0.425, 0, -pi/2])
-    dh_param5 = np.array([0, 0, pi/2])
+    dh_param3 = np.array([0.0534, 0, pi / 2])
+    dh_param4 = np.array([0.425, 0, -pi / 2])
+    dh_param5 = np.array([0, 0, pi / 2])
     dh_param6 = np.array([0.1, 0, 0])
     dh_param7 = np.array([0.1031, 0.17298, 0])
 
     T12 = Homgm(dh_param1, q1, offset=0)
-    T23 = Homgm(dh_param2, q2, offset=-pi/2)
-    T34 = Homgm(dh_param3, q3, offset=pi/2)
+    T23 = Homgm(dh_param2, q2, offset=-pi / 2)
+    T34 = Homgm(dh_param3, q3, offset=pi / 2)
     T45 = Homgm(dh_param4, q4, offset=0)
     T56 = Homgm(dh_param5, q5, offset=0)
     T67 = Homgm(dh_param6, q6, offset=0)
     T7E = Homgm(dh_param7, 0, offset=0)
 
-    TF = T12@T23@T34@T45@T56@T67@T7E
-    p = TF[:3,-1]
+    TF = T12 @ T23 @ T34 @ T45 @ T56 @ T67 @ T7E
+    p = TF[:3, -1]
     # print(p)
     return TF
 
-def simple_pseudo(pos_num, q0, p_goal, time_step=0.5, max_iteration=500000, accuracy=0.001):
 
-    Ktheta = np.diag(np.array([1.7, 5.9, 1.8, 0.29, 0.93 ,0.49]))
+def simple_pseudo(
+    pos_num, q0, p_goal, time_step=0.1, max_iteration=500000, accuracy=0.001
+):
+
+    Ktheta = np.diag(np.array([1.7, 5.9, 1.8, 0.29, 0.93, 0.49]))
     Ktheta_inv = np.linalg.inv(Ktheta)
-    F      = np.array([0.0, 0.0, 40.0, 0.0, 0.0, 0.0])
-
-    #goal_R = quaternion_matrix(p_goal[3:6])
-    # euler = euler_from_rotation(goal_R) # roll pitch yaw
+    F = np.array([0.0, 0.0, 40.0, 0.0, 0.0, 0.0])
     goal_R = rotation_from_euler(np.deg2rad(p_goal[3:6]))
-    # g_r31 = -sin(p_goal[4])
-    # g_r32 = cos(p_goal[4])*sin(p_goal[3])
-    # g_r21 = sin(p_goal[5])*cos(p_goal[4])
-    #print("goal euler: ", np.rad2deg(euler))
-    # print("Goal R: ", goal_R)
-    # print(g_r31, g_r32, g_r21)
-    # Setting initial variables
+
     q_n0 = q0
 
-    p = FK(q_n0)[:3,-1]
-    R = FK(q_n0)[:3, :-1] # Rotation matrix
-    
-    p_goal[3] = goal_R[2][1]
-    p_goal[4] = goal_R[2][0]
-    p_goal[5] = goal_R[1][0] # p_goal[3,4,5] = R31, R32, R21
-    print(p_goal[3], p_goal[4], p_goal[5])
-    p = np.array([ p[0], p[1], p[2], R[2][1], R[2][0], R[1][0]]) # shape miss match (6,1) # x,y,z R31, R32
+    p = FK(q_n0)[:3, -1]
+    R = FK(q_n0)[:3, :-1]  # Rotation matrix
 
-    t_dot = p_goal[:6]  - p
+    p_goal[3] = goal_R[2][1] # roll
+    p_goal[4] = goal_R[2][0] # pitch
+    p_goal[5] = goal_R[1][0] # yaw 
+
+    p = np.array(
+        [p[0], p[1], p[2], R[2][1], R[2][0], R[1][0]]
+    )  # shape miss match (6,1) # x,y,z R31, R32
+
+    t_dot = p_goal[:6] - p
     δt = time_step
-    i=0
+    i = 0
 
     start_time = time.time()
-    J_func    = dill.load(open(root+'/scripts/J_func_simp', "rb"))
-    print(root+'/scripts/J_func_simp')
+    #J_func = dill.load(open(root + '/scripts/J_func_simp', "rb"))
+    J_func = dill.load(open('../param_save/J_func_simp', "rb"))
+
     print("start runnign\n")
     q_dot = np.array([0, 0, 0, 0, 0, 0, 0])
     while True:
         if is_success(t_dot):
             print(f"Accuracy of {accuracy} reached")
             break
-        q_n0 = Joint_limit_check(q_n0) 
-        q_n0 = q_n0 + (δt * q_dot) 
+        q_n0 = Joint_limit_check(q_n0)
+        q_n0 = q_n0 + (δt * q_dot)
 
-        p = FK(q_n0)[:3,-1]
-        R = FK(q_n0)[:3,:-1] # Rotation matrix
-        r31, r32, r21 = wrpy(q_n0)
-        p = np.array([ p[0], p[1], p[2], R[2][1], R[2][0], R[1][0]]) # shape miss match (6,1) # x,y,z R31, R32
+        p = FK(q_n0)[:3, -1]
+        R = FK(q_n0)[:3, :-1]  # Rotation matrix
+
+        p = np.array(
+            [p[0], p[1], p[2], R[2][1], R[2][0], R[1][0]]
+        )  # shape miss match (6,1) # x,y,z R31, R32
 
         T = find_T(R)
         invT = np.linalg.inv(T)
 
         J = J_func(q_n0)
-        J_a = np.block([[np.eye(3), np.zeros((3,3))],[np.zeros((3, 3)), invT]]) @ J
-        J_inv = np.linalg.pinv(J_a) 
+        J_a = np.block([[np.eye(3), np.zeros((3, 3))], [np.zeros((3, 3)), invT]]) @ J
+        J_inv = np.linalg.pinv(J_a)
 
         t_dot = p_goal[:6] - p
-        #c = np.array([0.01,0.01,0.01,0.01,0.01,0.01])
+        # c = np.array([0.01,0.01,0.01,0.01,0.01,0.01])
 
-        J_temp =J[:,:6]@Ktheta_inv@J[:,:6].T
-        dxyz = J_temp@F[:6] 
-        #psd_J = J_a.T@ np.linalg.inv((J_a@J_a.T))#  + c.T@np.eye(6)
-        q_dot = (J_inv @ t_dot)
-        i+=1
-        if i % 100 ==0:
+        J_temp = J[:, :6] @ Ktheta_inv @ J[:, :6].T
+        dxyz = J_temp @ F[:6]
+        # psd_J = J_a.T@ np.linalg.inv((J_a@J_a.T))#  + c.T@np.eye(6)
+        q_dot = J_inv @ t_dot
+        i += 1
+        if i % 100 == 0:
             print(pos_num, i, " t_dot: ", t_dot.T)
-            print("devi :", dxyz)
-        if (i > max_iteration):
+            # print("devi :", dxyz)
+        if i > max_iteration:
             print("No convergence")
             break
     print("R : ", R)
     print("Goal R: ", goal_R)
     print("    ")
     rpy = euler_from_rotation(R)
-    #print("ans quat : ",quaternion_matrix(R))
-    #print("rpy angs : ", np.rad2deg(rpy))
-    p[3] = np.rad2deg(rpy[0]) # pitch roll yaw
+    # print("ans quat : ",quaternion_matrix(R))
+    # print("rpy angs : ", np.rad2deg(rpy))
+    p[3] = np.rad2deg(rpy[0])  # pitch roll yaw
     p[4] = np.rad2deg(rpy[1])
     p[5] = np.rad2deg(rpy[2])
     print(p[3], p[4], p[5])
     return q_n0, p, dxyz
+
 
 def get_cnfs_null(method_fun, q0, kwargs=dict()):
     # pos = np.array([0.650, -0.35, -0.278, 0, 1.57079, 0])
     # q, p, d_xyz = method_fun(0, q0, pos, **kwargs)
     # print(0, " pos", p, "\n ans : ", np.rad2deg(q))
     overallpos = posture_read()
-    J1, J2, J3, J4, J5, J6, index, pos, dx,dy,dz = ([] for i in range(11))
+    J1, J2, J3, J4, J5, J6, index, pos, dx, dy, dz = ([] for i in range(11))
     for i in range(len(overallpos)):
         q, p, d_xyz = method_fun(i, q0, np.array(overallpos[i]), **kwargs)
         print(i, " pos", p, "\n ans : ", np.rad2deg(q))
@@ -194,8 +210,28 @@ def get_cnfs_null(method_fun, q0, kwargs=dict()):
         dx.append(d_xyz[0])
         dy.append(d_xyz[1])
         dz.append(d_xyz[2])
-    pos_record = pd.DataFrame({'J1':np.rad2deg(J1), 'J2':np.rad2deg(J2), 'J3':np.rad2deg(J3), 'J4':np.rad2deg(J4), 'J5':np.rad2deg(J5), 'J6':np.rad2deg(J6), 'pos':pos, 'dx':dx, 'dy':dy, 'dz':dz}, index=index)
-    pos_record.to_excel('../data/non_optimized_curved.xlsx', sheet_name='Sheet2', float_format="%.3f", header=True)
+    pos_record = pd.DataFrame(
+        {
+            "J1": np.rad2deg(J1),
+            "J2": np.rad2deg(J2),
+            "J3": np.rad2deg(J3),
+            "J4": np.rad2deg(J4),
+            "J5": np.rad2deg(J5),
+            "J6": np.rad2deg(J6),
+            "pos": pos,
+            "dx": dx,
+            "dy": dy,
+            "dz": dz,
+        },
+        index=index,
+    )
+    pos_record.to_excel(
+        "../data/non_optimized_curved.xlsx",
+        sheet_name="Sheet2",
+        float_format="%.3f",
+        header=True,
+    )
+
 
 if __name__ == "__main__":
     # Length of Links in meters
@@ -204,4 +240,4 @@ if __name__ == "__main__":
     # q1 = np.array([0, 0, 90, 0, 0, 0, 0])
     # q1 = np.deg2rad(q1)
     # FK(q1)
-    get_cnfs_null(method_fun=simple_pseudo, q0=np.deg2rad([10 ,10, 10, 10, 10, 10, 0]))
+    get_cnfs_null(method_fun=simple_pseudo, q0=np.deg2rad([10, 10, 10, 10, 10, 10, 0]))

conven_control/scripts/spatialmath.py
@@ -1,8 +1,8 @@
-
 import time
 import numpy as np
 import sympy as sym
 from math import *
+
 sym.init_printing()
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
 from scipy.spatial.transform import Rotation as R
@@ -10,44 +10,62 @@ from scipy.spatial.transform import Rotation as R
 _NEXT_AXIS = [1, 2, 0, 1]
 _EPS = np.finfo(float).eps * 4.0
 _AXES2TUPLE = {
-    'sxyz': (0, 0, 0, 0), 'sxyx': (0, 0, 1, 0), 'sxzy': (0, 1, 0, 0),
-    'sxzx': (0, 1, 1, 0), 'syzx': (1, 0, 0, 0), 'syzy': (1, 0, 1, 0),
-    'syxz': (1, 1, 0, 0), 'syxy': (1, 1, 1, 0), 'szxy': (2, 0, 0, 0),
-    'szxz': (2, 0, 1, 0), 'szyx': (2, 1, 0, 0), 'szyz': (2, 1, 1, 0),
-    'rzyx': (0, 0, 0, 1), 'rxyx': (0, 0, 1, 1), 'ryzx': (0, 1, 0, 1),
-    'rxzx': (0, 1, 1, 1), 'rxzy': (1, 0, 0, 1), 'ryzy': (1, 0, 1, 1),
-    'rzxy': (1, 1, 0, 1), 'ryxy': (1, 1, 1, 1), 'ryxz': (2, 0, 0, 1),
-    'rzxz': (2, 0, 1, 1), 'rxyz': (2, 1, 0, 1), 'rzyz': (2, 1, 1, 1)}
+    "sxyz": (0, 0, 0, 0),
+    "sxyx": (0, 0, 1, 0),
+    "sxzy": (0, 1, 0, 0),
+    "sxzx": (0, 1, 1, 0),
+    "syzx": (1, 0, 0, 0),
+    "syzy": (1, 0, 1, 0),
+    "syxz": (1, 1, 0, 0),
+    "syxy": (1, 1, 1, 0),
+    "szxy": (2, 0, 0, 0),
+    "szxz": (2, 0, 1, 0),
+    "szyx": (2, 1, 0, 0),
+    "szyz": (2, 1, 1, 0),
+    "rzyx": (0, 0, 0, 1),
+    "rxyx": (0, 0, 1, 1),
+    "ryzx": (0, 1, 0, 1),
+    "rxzx": (0, 1, 1, 1),
+    "rxzy": (1, 0, 0, 1),
+    "ryzy": (1, 0, 1, 1),
+    "rzxy": (1, 1, 0, 1),
+    "ryxy": (1, 1, 1, 1),
+    "ryxz": (2, 0, 0, 1),
+    "rzxz": (2, 0, 1, 1),
+    "rxyz": (2, 1, 0, 1),
+    "rzyz": (2, 1, 1, 1),
+}
 
 
 def wrpy(q):
-  q1 = q[0]
-  q2 = q[1]
-  q3 = q[2]
-  q4 = q[3]
-  q5 = q[4]
-  q6 = q[5]
-  c1 = cos(q1)
-  c2 = cos(q2)
-  c3 = cos(q3)
-  c4 = cos(q4)
-  c5 = cos(q5)
-  c6 = cos(q6)
-  s1 = sin(q1)
-  s2 = sin(q2)
-  s3 = sin(q3)
-  s4 = sin(q4)
-  s5 = sin(q5)
-  s6 = sin(q6) 
-  c23 = cos(q2+q3)
-  s23 = sin(q2+q3)
+    q1 = q[0]
+    q2 = q[1]
+    q3 = q[2]
+    q4 = q[3]
+    q5 = q[4]
+    q6 = q[5]
+    c1 = cos(q1)
+    c2 = cos(q2)
+    c3 = cos(q3)
+    c4 = cos(q4)
+    c5 = cos(q5)
+    c6 = cos(q6)
+    s1 = sin(q1)
+    s2 = sin(q2)
+    s3 = sin(q3)
+    s4 = sin(q4)
+    s5 = sin(q5)
+    s6 = sin(q6)
+    c23 = cos(q2 + q3)
+    s23 = sin(q2 + q3)
 
-  r31 = (-s23)*c4*s5+(c23)*c5
-  r32 = -((-s23)*c4*c5-(c23)*s5)*s6-(-s23)*s4*c6
-  r21 = ((s1*c23)*c4+c1*s4)*s5+(s1*s23)*c5
-  return r31, r32, r21
-  
-def quaternion_from_euler(ai, aj, ak, axes='sxyz'):
+    r31 = (-s23) * c4 * s5 + (c23) * c5
+    r32 = -((-s23) * c4 * c5 - (c23) * s5) * s6 - (-s23) * s4 * c6
+    r21 = ((s1 * c23) * c4 + c1 * s4) * s5 + (s1 * s23) * c5
+    return r31, r32, r21
+
+
+def quaternion_from_euler(ai, aj, ak, axes="sxyz"):
     """Return quaternion from Euler angles and axis sequence.
     ai, aj, ak : Euler's roll, pitch and yaw angles
     axes : One of 24 axis sequences as string or encoded tuple
@@ -62,8 +80,8 @@ def quaternion_from_euler(ai, aj, ak, axes='sxyz'):
         firstaxis, parity, repetition, frame = axes
 
     i = firstaxis
-    j = _NEXT_AXIS[i+parity]
-    k = _NEXT_AXIS[i-parity+1]
+    j = _NEXT_AXIS[i + parity]
+    k = _NEXT_AXIS[i - parity + 1]
 
     if frame:
         ai, ak = ak, ai
@@ -79,28 +97,29 @@ def quaternion_from_euler(ai, aj, ak, axes='sxyz'):
     sj = sin(aj)
     ck = cos(ak)
     sk = sin(ak)
-    cc = ci*ck
-    cs = ci*sk
-    sc = si*ck
-    ss = si*sk
+    cc = ci * ck
+    cs = ci * sk
+    sc = si * ck
+    ss = si * sk
 
-    quaternion = numpy.empty((4, ), dtype=numpy.float64)
+    quaternion = numpy.empty((4,), dtype=numpy.float64)
     if repetition:
-        quaternion[i] = cj*(cs + sc)
-        quaternion[j] = sj*(cc + ss)
-        quaternion[k] = sj*(cs - sc)
-        quaternion[3] = cj*(cc - ss)
+        quaternion[i] = cj * (cs + sc)
+        quaternion[j] = sj * (cc + ss)
+        quaternion[k] = sj * (cs - sc)
+        quaternion[3] = cj * (cc - ss)
     else:
-        quaternion[i] = cj*sc - sj*cs
-        quaternion[j] = cj*ss + sj*cc
-        quaternion[k] = cj*cs - sj*sc
-        quaternion[3] = cj*cc + sj*ss
+        quaternion[i] = cj * sc - sj * cs
+        quaternion[j] = cj * ss + sj * cc
+        quaternion[k] = cj * cs - sj * sc
+        quaternion[3] = cj * cc + sj * ss
     if parity:
         quaternion[j] *= -1
 
     return quaternion
 
-def euler_from_matrix(matrix, axes='sxyz'):
+
+def euler_from_matrix(matrix, axes="sxyz"):
     """Return Euler angles from rotation matrix for specified axis sequence.
     axes : One of 24 axis sequences as string or encoded tuple
     Note that many Euler angle triplets can describe one matrix.
@@ -122,29 +141,29 @@ def euler_from_matrix(matrix, axes='sxyz'):
         firstaxis, parity, repetition, frame = axes
 
     i = firstaxis
-    j = _NEXT_AXIS[i+parity]
-    k = _NEXT_AXIS[i-parity+1]
+    j = _NEXT_AXIS[i + parity]
+    k = _NEXT_AXIS[i - parity + 1]
 
     M = np.array(matrix, dtype=np.float64, copy=False)[:3, :3]
     if repetition:
-        sy = sqrt(M[i, j]*M[i, j] + M[i, k]*M[i, k])
+        sy = sqrt(M[i, j] * M[i, j] + M[i, k] * M[i, k])
         if sy > _EPS:
-            ax = atan2( M[i, j],  M[i, k])
-            ay = atan2( sy,       M[i, i])
-            az = atan2( M[j, i], -M[k, i])
+            ax = atan2(M[i, j], M[i, k])
+            ay = atan2(sy, M[i, i])
+            az = atan2(M[j, i], -M[k, i])
         else:
-            ax = atan2(-M[j, k],  M[j, j])
-            ay = atan2( sy,       M[i, i])
+            ax = atan2(-M[j, k], M[j, j])
+            ay = atan2(sy, M[i, i])
             az = 0.0
     else:
-        cy = sqrt(M[i, i]*M[i, i] + M[j, i]*M[j, i])
+        cy = sqrt(M[i, i] * M[i, i] + M[j, i] * M[j, i])
         if cy > _EPS:
-            ax = atan2( M[k, j],  M[k, k])
-            ay = atan2(-M[k, i],  cy)
-            az = atan2( M[j, i],  M[i, i])
+            ax = atan2(M[k, j], M[k, k])
+            ay = atan2(-M[k, i], cy)
+            az = atan2(M[j, i], M[i, i])
         else:
-            ax = atan2(-M[j, k],  M[j, j])
-            ay = atan2(-M[k, i],  cy)
+            ax = atan2(-M[j, k], M[j, j])
+            ay = atan2(-M[k, i], cy)
             az = 0.0
 
     if parity:
@@ -153,13 +172,14 @@ def euler_from_matrix(matrix, axes='sxyz'):
         ax, az = az, ax
     return ax, ay, az
 
+
 def quaternion_matrix(quaternion):
-    '''
+    """
     Return homogeneous rotation matrix from quaternion.
     R = quaternion_matrix([0.06146124, 0, 0, 0.99810947])
     numpy.allclose(R, rotation_matrix(0.123, (1, 0, 0)))
     True
-    '''
+    """
     _EPS = np.finfo(float).eps * 4.0
     print(quaternion)
     q = np.array(quaternion[:4], dtype=np.float64, copy=True)
@@ -168,260 +188,314 @@ def quaternion_matrix(quaternion):
         return np.identity(4)
     q *= sqrt(2.0 / nq)
     q = np.outer(q, q)
-    return np.array((
-        (1.0-q[1, 1]-q[2, 2],     q[0, 1]-q[2, 3],     q[0, 2]+q[1, 3], 0.0),
-        (    q[0, 1]+q[2, 3], 1.0-q[0, 0]-q[2, 2],     q[1, 2]-q[0, 3], 0.0),
-        (    q[0, 2]-q[1, 3],     q[1, 2]+q[0, 3], 1.0-q[0, 0]-q[1, 1], 0.0),
-        (                0.0,                 0.0,                 0.0, 1.0)
-        ), dtype=np.float64)
+    return np.array(
+        (
+            (1.0 - q[1, 1] - q[2, 2], q[0, 1] - q[2, 3], q[0, 2] + q[1, 3], 0.0),
+            (q[0, 1] + q[2, 3], 1.0 - q[0, 0] - q[2, 2], q[1, 2] - q[0, 3], 0.0),
+            (q[0, 2] - q[1, 3], q[1, 2] + q[0, 3], 1.0 - q[0, 0] - q[1, 1], 0.0),
+            (0.0, 0.0, 0.0, 1.0),
+        ),
+        dtype=np.float64,
+    )
+
 
 def rad2deg(q):
-  return q*180/pi
+    return q * 180 / pi
+
 
 def deg2rad(q):
-  return q*pi/180
+    return q * pi / 180
+
 
 def find_T(R):
     rpy = euler_from_rotation(R)
     r = rpy[0]
     b = rpy[1]
     yaw = rpy[2]
-    T = np.array([ 
-        [1, 0, sin(b)], 
-        [0, cos(r), -cos(b)*sin(r)], 
-        [0, sin(r), cos(b)*cos(r)]
-    ])
+    T = np.array(
+        [[1, 0, sin(b)], [0, cos(r), -cos(b) * sin(r)], [0, sin(r), cos(b) * cos(r)]]
+    )
     return T
 
+
 def rotation_from_euler(euler):
-  '''
+    """
   rotation matrxi from euler
   Input : rx, ry, rz
   Output : Roation matrix Rz@Ry@Rx
-  '''
-  R = Rz(euler[2]) @ Ry(euler[1]) @ Rx(euler[0])
-  return R
+  """
+    R = Rz(euler[2]) @ Ry(euler[1]) @ Rx(euler[0])
+    return R
+
 
 def euler_from_rotation(R, sndSol=True):
-    '''
+    """
     Rotation to euler angle
     Input  : Rotation matrix
     Output : roll, pitch, yaw
-    '''
-    #rx = atan2(R[2,0], R[2,1])
-    rx = atan2(R[2,1], R[2,2]) 
-    ry = atan2(-R[2,0], sqrt(R[2,1]*R[2,1]+R[2,2]*R[2,2]))
-    #ry = atan2(sqrt(R[0,2]**2 + R[1,2]**2), R[2,2])
-    #rz = atan2(R[0,2], -R[1,2])
-    rz = atan2(R[1,0], R[0,0])
+    """
+    # rx = atan2(R[2,0], R[2,1])
+    rx = atan2(R[2, 1], R[2, 2])
+    ry = atan2(-R[2, 0], sqrt(R[2, 1] * R[2, 1] + R[2, 2] * R[2, 2]))
+    # ry = atan2(sqrt(R[0,2]**2 + R[1,2]**2), R[2,2])
+    # rz = atan2(R[0,2], -R[1,2])
+    rz = atan2(R[1, 0], R[0, 0])
     # r = np.rad2deg([rx,ry,rz])
     # print(r)
     return [rx, ry, rz]
 
+
 def Rx(q):
-  T = np.array([[1,         0,          0, 0],
-                [0, np.cos(q), -np.sin(q), 0],
-                [0, np.sin(q),  np.cos(q), 0],
-                [0,         0,          0, 1]], dtype=float)
-  return T
+    T = np.array(
+        [
+            [1, 0, 0, 0],
+            [0, np.cos(q), -np.sin(q), 0],
+            [0, np.sin(q), np.cos(q), 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=float,
+    )
+    return T
+
 
 def Ry(q):
-  T = np.array([[ np.cos(q), 0, np.sin(q), 0],
-                [         0, 1,         0, 0],
-                [-np.sin(q), 0, np.cos(q), 0],
-                [         0, 0,         0, 1]], dtype=float)
-  return T
+    T = np.array(
+        [
+            [np.cos(q), 0, np.sin(q), 0],
+            [0, 1, 0, 0],
+            [-np.sin(q), 0, np.cos(q), 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=float,
+    )
+    return T
+
 
 def Rz(q):
-  T = np.array([[np.cos(q), -np.sin(q), 0, 0],
-                [np.sin(q),  np.cos(q), 0, 0],
-                [        0,          0, 1, 0],
-                [        0,          0, 0, 1]], dtype=float)
-  return T
+    T = np.array(
+        [
+            [np.cos(q), -np.sin(q), 0, 0],
+            [np.sin(q), np.cos(q), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ],
+        dtype=float,
+    )
+    return T
+
 
 def d_Rx(q):
-  T = np.array([[0,          0,          0, 0],
-                [0, -np.sin(q), -np.cos(q), 0],
-                [0,  np.cos(q), -np.sin(q), 0],
-                [0,          0,          0, 0]], dtype=float)
-  return T
+    T = np.array(
+        [
+            [0, 0, 0, 0],
+            [0, -np.sin(q), -np.cos(q), 0],
+            [0, np.cos(q), -np.sin(q), 0],
+            [0, 0, 0, 0],
+        ],
+        dtype=float,
+    )
+    return T
+
 
 def d_Ry(q):
-  T = np.array([[-np.sin(q), 0,  np.cos(q), 0],
-                [         0, 0,          0, 0],
-                [-np.cos(q), 0, -np.sin(q), 0],
-                [         0, 0,          0, 0]], dtype=float)
-  return T
+    T = np.array(
+        [
+            [-np.sin(q), 0, np.cos(q), 0],
+            [0, 0, 0, 0],
+            [-np.cos(q), 0, -np.sin(q), 0],
+            [0, 0, 0, 0],
+        ],
+        dtype=float,
+    )
+    return T
+
 
 def d_Rz(q):
-  T = np.array([[-np.sin(q), -np.cos(q), 0, 0],
-                [ np.cos(q), -np.sin(q), 0, 0],
-                [         0,          0, 0, 0],
-                [         0,          0, 0, 0]], dtype=float)
-  return T
+    T = np.array(
+        [
+            [-np.sin(q), -np.cos(q), 0, 0],
+            [np.cos(q), -np.sin(q), 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ],
+        dtype=float,
+    )
+    return T
+
 
 def Rx_sym(q):
-  return sym.Matrix(
-      [[1, 0, 0, 0],
-        [0, sym.cos(q), -sym.sin(q), 0],
-        [0, sym.sin(q), sym.cos(q), 0],
-        [0, 0, 0, 1]]
-  )
+    return sym.Matrix(
+        [
+            [1, 0, 0, 0],
+            [0, sym.cos(q), -sym.sin(q), 0],
+            [0, sym.sin(q), sym.cos(q), 0],
+            [0, 0, 0, 1],
+        ]
+    )
+
 
 def Ry_sym(q):
-  return sym.Matrix(
-      [[sym.cos(q), 0, sym.sin(q), 0],
-        [0, 1, 0, 0],
-        [-sym.sin(q), 0, sym.cos(q), 0],
-        [0, 0, 0, 1]]
-  )
+    return sym.Matrix(
+        [
+            [sym.cos(q), 0, sym.sin(q), 0],
+            [0, 1, 0, 0],
+            [-sym.sin(q), 0, sym.cos(q), 0],
+            [0, 0, 0, 1],
+        ]
+    )
+
 
 def Rz_sym(q):
-  return sym.Matrix(
-      [[sym.cos(q), -sym.sin(q), 0, 0],
-        [sym.sin(q), sym.cos(q), 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]]
-  )
+    return sym.Matrix(
+        [
+            [sym.cos(q), -sym.sin(q), 0, 0],
+            [sym.sin(q), sym.cos(q), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ]
+    )
 
 
 def d_Rx_sym(q):
-  return sym.Matrix(
-      [[0, 0, 0, 0],
-        [0, -sym.sin(q), -sym.cos(q), 0],
-        [0, sym.cos(q), -sym.sin(q), 0],
-        [0, 0, 0, 0]]
-  )
+    return sym.Matrix(
+        [
+            [0, 0, 0, 0],
+            [0, -sym.sin(q), -sym.cos(q), 0],
+            [0, sym.cos(q), -sym.sin(q), 0],
+            [0, 0, 0, 0],
+        ]
+    )
+
 
 def d_Ry_sym(q):
-  return sym.Matrix(
-      [[-sym.sin(q), 0, sym.cos(q), 0],
-        [0, 0, 0, 0],
-        [-sym.cos(q), 0, -sym.sin(q), 0],
-        [0, 0, 0, 0]]
-  )
+    return sym.Matrix(
+        [
+            [-sym.sin(q), 0, sym.cos(q), 0],
+            [0, 0, 0, 0],
+            [-sym.cos(q), 0, -sym.sin(q), 0],
+            [0, 0, 0, 0],
+        ]
+    )
+
 
 def d_Rz_sym(q):
-  return sym.Matrix(
-      [[-sym.sin(q), -sym.cos(q), 0, 0],
-        [sym.cos(q), -sym.sin(q), 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]])
+    return sym.Matrix(
+        [
+            [-sym.sin(q), -sym.cos(q), 0, 0],
+            [sym.cos(q), -sym.sin(q), 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+    )
+
 
 def Tx(x):
-  T = np.array([[1, 0, 0, x],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1]], dtype=float)
-  return T
+    T = np.array([[1, 0, 0, x], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=float)
+    return T
+
 
 def Ty(y):
-  T = np.array([[1, 0, 0, 0],
-                [0, 1, 0, y],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1]], dtype=float)
-  return T
+    T = np.array([[1, 0, 0, 0], [0, 1, 0, y], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=float)
+    return T
+
 
 def Tz(z):
-  T = np.array([[1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, z],
-                [0, 0, 0, 1]], dtype=float)
-  return T
+    T = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, z], [0, 0, 0, 1]], dtype=float)
+    return T
+
 
 def Tx_sym(s):
-  return sym.Matrix(
-      [[1, 0, 0, s],
-        [0, 1, 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]]
-  )
+    return sym.Matrix([[1, 0, 0, s], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+
 
 def Ty_sym(s):
-  return sym.Matrix(
-      [[1, 0, 0, 0],
-        [0, 1, 0, s],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1]]
-  )
+    return sym.Matrix([[1, 0, 0, 0], [0, 1, 0, s], [0, 0, 1, 0], [0, 0, 0, 1]])
+
 
 def Tz_sym(s):
-  return sym.Matrix(
-      [[1, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, 0, 1, s],
-        [0, 0, 0, 1]]
-  )
+    return sym.Matrix([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, s], [0, 0, 0, 1]])
+
 
 def d_Tx(x):
-  T = np.array([[0, 0, 0, 1],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0]], dtype=float)
-  return T
+    T = np.array([[0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=float)
+    return T
+
 
 def d_Ty(y):
-  T = np.array([[0, 0, 0, 0],
-                [0, 0, 0, 1],
-                [0, 0, 0, 0],
-                [0, 0, 0, 0]], dtype=float)
-  return T
+    T = np.array([[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0]], dtype=float)
+    return T
+
 
 def d_Tz(z):
-  T = np.array([[0, 0, 0, 0],
-                [0, 0, 0, 0],
-                [0, 0, 0, 1],
-                [0, 0, 0, 0]], dtype=float)
-  return T
+    T = np.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]], dtype=float)
+    return T
+
 
 def d_Tx_sym():
-  return sym.Matrix(
-      [[0, 0, 0, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]]
-  )
+    return sym.Matrix([[0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+
 
 def d_Ty_sym():
-  return sym.Matrix(
-      [[0, 0, 0, 0],
-        [0, 0, 0, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]]
-  )
+    return sym.Matrix([[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0], [0, 0, 0, 0]])
+
 
 def d_Tz_sym():
-  return sym.Matrix(
-      [[0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 1],
-        [0, 0, 0, 0]]
-  )
+    return sym.Matrix([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 0]])
+
 
 def Homgm(dh_param, q, offset=0):
-  d = dh_param[0]
-  a = dh_param[1]
-  alpha = dh_param[2]
-  q = q + offset
+    d = dh_param[0]
+    a = dh_param[1]
+    alpha = dh_param[2]
+    q = q + offset
 
-  T = np.array([[np.cos(q), -np.cos(alpha)*np.sin(q), np.sin(alpha)*np.sin(q), a*np.cos(q)],
-                [np.sin(q), np.cos(alpha)*np.cos(q),  -np.sin(alpha)*np.cos(q), a*np.sin(q)],
-                [0, np.sin(alpha), np.cos(alpha), d],
-                [0,0,0,1]], dtype=float)
+    T = np.array(
+        [
+            [
+                np.cos(q),
+                -np.cos(alpha) * np.sin(q),
+                np.sin(alpha) * np.sin(q),
+                a * np.cos(q),
+            ],
+            [
+                np.sin(q),
+                np.cos(alpha) * np.cos(q),
+                -np.sin(alpha) * np.cos(q),
+                a * np.sin(q),
+            ],
+            [0, np.sin(alpha), np.cos(alpha), d],
+            [0, 0, 0, 1],
+        ],
+        dtype=float,
+    )
 
-  return T
+    return T
+
 
 def Homgm_sym(dh_param, q, offset=0):
-  d = dh_param[0]
-  a = dh_param[1]
-  alpha = dh_param[2]
+    d = dh_param[0]
+    a = dh_param[1]
+    alpha = dh_param[2]
 
-  q = q + offset
+    q = q + offset
 
-  T = sym.Matrix([[sym.cos(q), -sym.cos(alpha)*sym.sin(q), sym.sin(alpha)*sym.sin(q), a*sym.cos(q)],
-                [sym.sin(q), sym.cos(alpha)*sym.cos(q),  -sym.sin(alpha)*sym.cos(q), a*sym.sin(q)],
-                [0, sym.sin(alpha), sym.cos(alpha), d],
-                [0,0,0,1]])
+    T = sym.Matrix(
+        [
+            [
+                sym.cos(q),
+                -sym.cos(alpha) * sym.sin(q),
+                sym.sin(alpha) * sym.sin(q),
+                a * sym.cos(q),
+            ],
+            [
+                sym.sin(q),
+                sym.cos(alpha) * sym.cos(q),
+                -sym.sin(alpha) * sym.cos(q),
+                a * sym.sin(q),
+            ],
+            [0, sym.sin(alpha), sym.cos(alpha), d],
+            [0, 0, 0, 1],
+        ]
+    )
 
-  return T
-
+    return T