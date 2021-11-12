f7cf6554a021879e8876baa74291fb128040f25f
benthebear93 <nswve@naver.com>
2021-11-12 17:54:04 +0900

    update

--- FRF/frf_analysis.py
+++ FRF/frf_analysis.py
@@ -35,11 +35,11 @@ class FRF:
         self.avg_Amp = moving_average(self.Amp, 30)
         stiff = 1/self.dis
 
-        self.dis_max = max(self.avg_dis[:400])
-        self.dis_index = self.Freq[np.argmax(self.avg_dis[:400])]
+        self.dis_max = max(self.avg_dis[80:400])
+        self.dis_index = self.Freq[np.argmax(self.avg_dis[80:400])]
 
-        self.Amp_max = max(self.avg_Amp[:400])
-        self.Amp_index = self.Freq[np.argmax(self.avg_Amp[:400])]
+        self.Amp_max = max(self.avg_Amp[80:400])
+        self.Amp_index = self.Freq[np.argmax(self.avg_Amp[80:400])]
         #self.index = self.Freq[np.argmax(stiff)]
 
 def convert_to_stiffness(filename, axis):
@@ -173,7 +173,7 @@ def compare_frf(filename, axis):
         ax.plot(non_opt.Freq[:400], non_opt.avg_Amp[:400], label='Non optimized') #1024 - avg_size
         ax.plot(opt.Freq[:400], opt.avg_Amp[:400], label='optimized')
         ax.grid()
-        plt.legend(loc='upper right', ncol=1)
+        plt.legend(loc='upper right', ncol=1) 
         plt.savefig(title+'.png')
         #plt.show()
 
@@ -181,61 +181,93 @@ def compare_frf(filename, axis):
 
 def compare_compliance(filename, axis):
     file_path = os.getcwd()
-    file_path = file_path + "/test2"
+    file_path = file_path + "/curve"
     # print("file path :", file_path)
     non_opt_max_list =[]
     opt_max_list =[]
     # print("filename: ", filename)
     optimized_number  = 0
     for i in range(0, len(filename), 2):
-        print("here ", i)
         non_opt = FRF(file_path + filename[i])
         opt = FRF(file_path + filename[i+1])
         non_opt.readExcel()
         opt.readExcel()
-        print("max: ", non_opt.max)
         if opt.max >= non_opt.max:
-            print("optimized_number: ", optimized_number)
             optimized_number+=1
-        non_opt_max_list.append(non_opt.max)
-        opt_max_list.append(opt.max)
+        non_opt_max_list.append(non_opt.dis_max)
+        opt_max_list.append(opt.dis_max)
 
-        fig, ax = plt.subplots()
-        plt.setp(ax.spines.values(), linewidth=1.7)
-        title = "Impact hammer test Result of " + axis + str(1+round(i/2,1))
-        ax.set_title(title, fontsize=17)
-        ax.set_xlabel('Frequency [Hz]', fontsize = 16)
-        ax.set_ylabel('Compliance[m/N]', fontsize = 16)
+        # fig, ax = plt.subplots()
+        # plt.setp(ax.spines.values(), linewidth=1.7)
+        # title = "Impact hammer test Result of " + axis + str(1+round(i/2,1))
+        # ax.set_title(title, fontsize=17)
+        # ax.set_xlabel('Frequency [Hz]', fontsize = 16)
+        # ax.set_ylabel('Compliance[m/N]', fontsize = 16)
 
-        ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
-        ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+        # ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+        # ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
 
-        ax.xaxis.set_ticks_position('both')
-        ax.yaxis.set_ticks_position('both')
+        # ax.xaxis.set_ticks_position('both')
+        # ax.yaxis.set_ticks_position('both')
 
-        #ax.yaxis.set_tick_params(which='minor',direction="in")
-        ax.tick_params(axis="x", direction="in", which='major', labelsize=13, width=2)
-        ax.tick_params(axis="x", direction="in", which='minor', labelsize=13)
+        # ax.scatter(non_opt.dis_index+40, non_opt.dis_max)
+        # ax.scatter(opt.dis_index+40, opt.dis_max)
 
-        ax.tick_params(axis="y", direction="in", which='major', labelsize=13, width=2)
-        ax.tick_params(axis="y", direction="in", which='minor', labelsize=13)
-        plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        # #ax.yaxis.set_tick_params(which='minor',direction="in")
+        # ax.tick_params(axis="x", direction="in", which='major', labelsize=13, width=2)
+        # ax.tick_params(axis="x", direction="in", which='minor', labelsize=13)
 
-        ax.set_xlim(xmin=0, xmax=200)
-        ax.set_ylim(ymin=0, ymax=1e-4)
-        print(len(non_opt.avg_dis))
-        print(len(non_opt.Freq)) 
-        size_set = len(non_opt.avg_dis)
-        ax.plot(non_opt.Freq[:size_set], non_opt.avg_dis, label='Non optimized') #1024 - avg_size
-        ax.plot(opt.Freq[:size_set], opt.avg_dis, label='optimized')
-        ax.grid()
-        plt.legend(loc='upper right', ncol=1)
-        plt.savefig(title+'.png')
+        # ax.tick_params(axis="y", direction="in", which='major', labelsize=13, width=2)
+        # ax.tick_params(axis="y", direction="in", which='minor', labelsize=13)
+        # plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+
+        # ax.set_xlim(xmin=0, xmax=200)
+        # ax.set_ylim(ymin=0, ymax=1e-4)
+        # print(len(non_opt.avg_dis))
+        # print(len(non_opt.Freq)) 
+        # size_set = len(non_opt.avg_dis)
+        # ax.plot(non_opt.Freq[:size_set], non_opt.avg_dis[:], label='Non optimized') #1024 - avg_size
+        # ax.plot(opt.Freq[:size_set], opt.avg_dis[:], label='optimized')
+        # ax.grid()
+        # plt.legend(loc='upper right', ncol=1)
+        # # plt.savefig(title+'.png')
         # plt.show()
+    print("here?")
+    fig, ax = plt.subplots()
+    plt.setp(ax.spines.values(), linewidth=1.7)
+    index = np.arange(12)
+    width =0.27
+
+    ax.set_title("Impact hammer test result " + axis, fontsize=17)
+    ax.set_xlabel('Posture'  , fontsize = 16)
+    ax.set_ylabel('Compliance [m/N]', fontsize = 16)
+
+    ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+    ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    #ax.yaxis.set_tick_params(which='minor',direction="in")
+    ax.tick_params(axis="x", direction="in", which='major', labelsize=13, width=2)
+    ax.tick_params(axis="x", direction="in", which='minor', labelsize=13)
+
+    ax.tick_params(axis="y", direction="in", which='major', labelsize=13, width=2)
+    ax.tick_params(axis="y", direction="in", which='minor', labelsize=13)
+    print("here?2")
+    print("shape:", len(index), len(non_opt_max_list))
+    rect1 = ax.bar(index, non_opt_max_list, width, color = 'r' )
+    rect2 = ax.bar(index+width, opt_max_list, width, color = 'b' )
+    
+    ax.set_axisbelow(True)
+    ax.grid()
+    ax.legend( (rect1[0], rect2[0]), ('Non_optimized', 'optimized') )
+    plt.show()
+    return non_opt_max_list, opt_max_list
+
 
 if __name__ == "__main__":
     filename_y = []
-    number_of_pose = 15
+    number_of_pose = 12
     fileformat = ".xlsx"
 
     for i in range(1,number_of_pose+1):
@@ -256,8 +288,42 @@ if __name__ == "__main__":
     #non_x, opt_x = convert_to_stiffness(filename_x, "x ")
     # non_y, opt_y = compare_frf(filename_y, "y")
     # non_x, opt_x = compare_frf(filename_x, "x")
-    compare_compliance(filename_y, "y")
-    compare_compliance(filename_x, "x")
+    non_opt_comp_y, opt_comp_y = compare_compliance(filename_y, "y")
+    non_opt_comp_x, opt_comp_x = compare_compliance(filename_x, "x")
+
+    non_opt_sum, opt_sum = [], []
+    for i in range(len(non_opt_comp_x)):
+        non_opt_sum.append(non_opt_comp_y[i] +  non_opt_comp_x[i])
+        opt_sum.append(opt_comp_y[i] +  opt_comp_x[i])
+
+    fig, ax = plt.subplots()
+    plt.setp(ax.spines.values(), linewidth=1.7)
+    index = np.arange(12)
+    width =0.27
+
+    ax.set_title("Impact hammer test result", fontsize=17)
+    ax.set_xlabel('Posture'  , fontsize = 16)
+    ax.set_ylabel('Sum of Compliance [m/N]', fontsize = 16)
+
+    ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+    ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    #ax.yaxis.set_tick_params(which='minor',direction="in")
+    ax.tick_params(axis="x", direction="in", which='major', labelsize=13, width=2)
+    ax.tick_params(axis="x", direction="in", which='minor', labelsize=13)
+
+    ax.tick_params(axis="y", direction="in", which='major', labelsize=13, width=2)
+    ax.tick_params(axis="y", direction="in", which='minor', labelsize=13)
+
+    rect1 = ax.bar(index, non_opt_sum, width, color = 'r' )
+    rect2 = ax.bar(index+width, opt_sum, width, color = 'b' )
+    
+    ax.set_axisbelow(True)
+    ax.grid()
+    ax.legend( (rect1[0], rect2[0]), ('Non_optimized', 'optimized') )
+    plt.show()
 
     # non_H, opt_H = [],[]
     # for i in range(len(non_y)):
